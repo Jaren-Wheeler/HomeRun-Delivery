a73530e7549b61d3a5b126b467a1b6ec428dca2b
Jaren-Wheeler <git@github.com>
2025-11-03 08:54:24 -0700

    Setup frontend and backend with basic server and file structure

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Server is running.')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
