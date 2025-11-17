afa8a1c29ede758d8d3a0d0685c9c9aa9409b809
Jaren-Wheeler <git@github.com>
2025-11-17 15:13:26 -0700

    Add more UI components to deliverer dashboard. Clean up navbar code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import PurchaserDashboard from './pages/PurchaserDashboard';
+import DelivererDashboard from './pages/DelivererDashboard';
+import Homepage from './pages/Homepage';
+import Payment from './pages/Payment';
+import NavBar from './components/NavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <Router>
+      <NavBar></NavBar>
+
+      <Routes>
+        {/*<Route path="/" element={<Homepage />} />*/}
+         {/*<Route path="/purchaser-dashboard" element={<PurchaserDashboard />} />*/}
+        <Route path="/deliverer-dashboard" element={<DelivererDashboard />} />
+         {/*<Route path="/payment" element={<Payment />} />*/}
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/pages/DelivererDashboard.js
+++ frontend/src/pages/DelivererDashboard.js
@@ -1,10 +1,30 @@
+<<<<<<< Updated upstream
 import {useState, useEffect} from 'react-router-dom';
 
+=======
+import {useState, useEffect} from 'react';
+import MapComponent from '../components/MapComponent';
+import DelivererOptions from '../components/DelivererOptions';
+>>>>>>> Stashed changes
 
 const delivererDashboard = () => {
 
     return (
+<<<<<<< Updated upstream
         <div class="map-container">
+=======
+        <div style={{display:'flex', flexDirection: 'column', height: '100vh', gap: '1rem'}}>
+            <div style={{height: '10vh', display: 'flex', flexDirection: 'column'}}>
+                <h2 style={{textAlign: "center"}}>Find Jobs</h2>
+                <div style={{alignSelf: "flex-end", marginRight: '1rem'}}>
+                    <DelivererOptions ></DelivererOptions>
+                </div>
+            </div>
+            <MapComponent style={{height: '90vh'}}></MapComponent>
+        </div>   
+    )
+}
+>>>>>>> Stashed changes
 
         </div>
     )
