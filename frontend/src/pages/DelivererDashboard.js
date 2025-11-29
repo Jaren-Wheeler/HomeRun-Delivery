3d095f70c01adfbf683fdb192cd06570fac299e0
Jaren-Wheeler <git@github.com>
2025-11-28 18:02:13 -0700

    Included searching functionality on map

--- frontend/src/pages/DelivererDashboard.js
+++ frontend/src/pages/DelivererDashboard.js
@@ -1,31 +1,49 @@
-import {useState, useEffect} from 'react';
+import { useState } from 'react';
 import MapComponent from '../components/MapComponent';
 import DelivererOptions from '../components/DelivererOptions';
-import RadiusSelector from '../components/RadiusSelector';
+
 
 const DelivererDashboard = () => {
+    const [autocomplete, setAutocomplete] = useState(null);
+    const [searchCenter, setSearchCenter] = useState(null);
+    const [selectedRadius, setSelectedRadius] = useState(20); // default 20 km
+
+    const onLoad = (auto) => {
+        setAutocomplete(auto);
+    };
+
+    const onPlaceChanged = () => {
+        if (!autocomplete) return;
+
+        const place = autocomplete.getPlace();
+        if (!place?.geometry) return;
+
+        setSearchCenter({
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+        });
+    };
 
     return (
-        <div style={{display:'flex', flexDirection: 'column', height: '100vh', gap: '1rem'}}>
-            <div style={{height: '10vh', display: 'flex', flexDirection: 'column'}}>
-                <h2 style={{textAlign: "center"}}>Find Jobs</h2>
-               
-               <div style={{display: 'flex', width: '100%', justifyContent: "space-between"}}>
-                    <div>
-                        <input type='text' placeholder="Search Locations" style={{alignSelf: 'flex-start', width: "25%", fontSize: "16px"}}></input>
-                        <RadiusSelector></RadiusSelector>
-                    </div>
-                 
-                    <div style={{alignSelf: "flex-end", marginRight: '1rem'}}>
-                        
-                        <DelivererOptions></DelivererOptions>
-                    </div>
-               </div>
+     
+            <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', gap: '1rem' }}>
                 
+                <div style={{ height: '10vh', display: 'flex', flexDirection: 'column' }}>
+                    <h2 style={{ textAlign: "center" }}>Find Jobs</h2>
+
+                    <div style={{ display: 'flex', width: '100%', justifyContent: "space-between" }}>
+                        <div style={{ alignSelf: "flex-end", marginRight: '1rem' }}>
+                            <DelivererOptions />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Map Component */}
+                <MapComponent searchCenter={searchCenter} style={{ height: '90vh' }} />
+
             </div>
-            <MapComponent style={{height: '90vh'}}></MapComponent>
-        </div>   
-    )
-}
+
+    );
+};
 
 export default DelivererDashboard;
