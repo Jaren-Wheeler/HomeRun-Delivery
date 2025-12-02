9a151067a0fa7f107edcfd3e0c10e5dce58e6ef3
Jaren-Wheeler <git@github.com>
2025-12-01 22:16:41 -0700

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,18 +5,27 @@ require('dotenv').config();
 
 // Sequelize object. Data sourced in environment variables
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_SERVER,
-        dialect: "mssql",
-        port: parseInt(process.env.DB_PORT, 10),
-        dialectOptions: {
-            options: { trustServerCertificate: true}
-        },
-        logging: false
-    }
-)
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  
+  {
+    dialect: "mssql",
+    dialectOptions: {
+      options: {
+        encrypt: false,
+        trustServerCertificate: true
+      }
+    },
+    server: process.env.DB_SERVER,   // <-- KEY FIX
+    port: 1433,
+    logging: false
+  }
+
+  
+);
+
+
+
 
 module.exports = sequelize;
